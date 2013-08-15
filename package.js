d267d4143ff5b787285a5ab78eeaacfc078426ea
Kevin Kaland <kevin@wizonesolutions.com>
2013-08-15 22:09:58 +0200

    Export analytics variable.
    This is for Meteor 0.6.5 support.

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
-    summary: "Analytics.js packaged for meteor."
+  summary: "Analytics.js packaged for meteor."
 });
 
 Package.on_use(function (api) {
-    api.add_files([
-        'loadAnalytics.js'
-    ], 'client'
-    );
+  api.add_files([
+    'loadAnalytics.js'
+  ], 'client'
+  );
+
+  api.export("analytics", "client");
 });
