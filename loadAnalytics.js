8f67f062e4e5c8471103f6613c99be8d63a2c041
Nathan Strauser <nathan.strauser@gmail.com>
2013-08-20 14:43:11 -0400

    add to window

--- loadAnalytics.js
+++ loadAnalytics.js
@@ -1,4 +1,5 @@
 analytics=[];
+window.analytics = analytics;
 var localAnalytics = [];
 // Define a method that will asynchronously load analytics.js from our CDN.
 localAnalytics.load = function(apiKey) {
@@ -8,11 +9,11 @@ localAnalytics.load = function(apiKey) {
 	script.async = true;
 	script.src = ('https:' === document.location.protocol ? 'https://' : 'http://') +
 	'd2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/' + apiKey + '/analytics.min.js';
-	
+
 	// Find the first script element on the page and insert our script next to it.
 	var firstScript = document.getElementsByTagName('script')[0];
 	firstScript.parentNode.insertBefore(script, firstScript);
-	
+
 	// Define a factory that generates wrapper methods to push arrays of
 	// arguments onto our `analytics` queue, where the first element of the arrays
 	// is always the name of the analytics.js method itself (eg. `track`).
@@ -21,7 +22,7 @@ localAnalytics.load = function(apiKey) {
 			localAnalytics.push([type].concat(Array.prototype.slice.call(arguments, 0)));
 		};
 	};
-	
+
 	// Loop through analytics.js' methods and generate a wrapper method for each.
 	var methods = ['identify', 'track', 'trackLink', 'trackForm', 'trackClick',
 	'trackSubmit', 'pageview', 'ab', 'alias', 'ready'];
@@ -31,6 +32,7 @@ localAnalytics.load = function(apiKey) {
 	analytics=localAnalytics;
 };
 analytics=localAnalytics;
+window.analytics = analytics;
 
 // Load analytics.js with your API key, which will automatically load all of the
 // analytics integrations you've turned on for your account. Boosh!
