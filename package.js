9a49dd713941d5a68a0e34c4ebf1217df1d1b676
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 20:42:03 -0400

    0.9.0 update

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-  summary: "Analytics.js packaged for meteor."
+  name:"natestrauser:analyticsjs",
+  summary: "Analytics.js packaged for meteor.",
+  version: "0.1.9",
+  git: "https://github.com/nate-strauser/meteor-analyticsjs.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   // CLIENT
   api.use([
 	'session'
